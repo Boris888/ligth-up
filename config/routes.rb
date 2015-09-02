0cf36f14f669dec31e0855f5accd58e2952a31a9
Boris Hruruguen <boris.huruguen@gmail.com>
2015-09-02 16:43:51 +0200

    peauffinement

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
 resources :courses
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#home'
+  root to: 'courses#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
