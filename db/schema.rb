43a70530b6c7cce85c1b1e219f7157cba6bad82b
Boris Hruruguen <boris.huruguen@gmail.com>
2015-09-02 10:55:49 +0200

    models crash testé ok

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901150811) do
+ActiveRecord::Schema.define(version: 20150902082047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chapters", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "chapters", ["course_id"], name: "index_chapters_on_course_id", using: :btree
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "price"
+    t.string   "description"
+    t.integer  "user_id"
+    t.string   "fichier_video"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "courses", ["user_id"], name: "index_courses_on_user_id", using: :btree
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "course_id"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "orders", ["course_id"], name: "index_orders_on_course_id", using: :btree
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -41,4 +73,8 @@ ActiveRecord::Schema.define(version: 20150901150811) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "chapters", "courses"
+  add_foreign_key "courses", "users"
+  add_foreign_key "orders", "courses"
+  add_foreign_key "orders", "users"
 end
