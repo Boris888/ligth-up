7709fb17acd10917a8b6eb7ae1561112ef2b2acc
Boris Hruruguen <boris.huruguen@gmail.com>
2015-09-02 20:04:40 +0200

    commit avant test de la seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,28 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-order1 = Order.create(price: 7)
-Order.create(price: 8)
-Order.create(price: 15)
+# Course.create(name:"théologie globale", user_id: User.first, description:"super", price: 3)
+# Course.create(name:"maitrisé super scraper", user_id: User.first, description:"génial", price: 2)
+# Course.create(name:"réalisez un smoothie parfait", user_id: User.first, description:"fantastique", price: 2)
+
+
+
+# Chapter.create(name:"track 22", course_id: 3)
+# Chapter.create(name:"tr 77", course_id: 5)
+# Chapter.create(name:"tr 78", course_id: 6)
+# Chapter.create(name:"tr 79", course_id: 6)
+
+# order1 = Order.create(price: 7)
+# Order.create(price: 8)
+# Order.create(price: 15)
+# Order.create(price: 5)
+# Order.create(price: 14 )
+# Order.create(price: 12 )
+
+
+courses_attributes.each { |params| Course.create!(params)}
+
+namba = Chapter.new(name: "tr 777")
+kendo = Course.first
+namba.course = kendo
+namba.save
